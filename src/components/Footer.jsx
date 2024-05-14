0cb0f746c8460cf7ac9cc732c3abd78e28b51b01
rupashreesmagic <rupashreesmagic@gmail.com>
2024-05-14 23:12:52 +0530

    first commit

---
+++ src/components/Footer.jsx
@@ -0,0 +1,23 @@
+import React from "react";
+
+
+const Footer = () => {
+   return <footer>
+    <div>
+      <h1>Tech World</h1>
+      <p>@all rights reserved</p>
+    </div>
+
+    <div>
+      <h5>Follow Us</h5>
+      <div>
+        <a href= "https://youtube.com/@rupashreescanvas2562?si=jH3YEKvjSl3B-OU5" target = {"blank"}>YouTube</a>
+        <a href = "https://www.instagram.com/rupashreesworld/" target = {"blank"}>Instagram</a>
+        <a href = "https://github.com/rupashreesmagic" target = {"blank"}>GitHub</a>
+      </div>
+    </div>
+
+   </footer>
+}
+
+export default Footer;
